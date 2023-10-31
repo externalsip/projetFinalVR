94afc1d1177ad369d06079a057522bd0194d606f
externalsip <93773873+externalsip@users.noreply.github.com>
2023-10-31 18:47:56 -0400

    tests (didnt work oops)

--- fishing roguelite/Assets/script/fishingRodTests.cs
+++ fishing roguelite/Assets/script/fishingRodTests.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+//public XR.DirectInteractor rHand;
+//public XR.DirectInteractor lHand;
+
 
 public class fishingRodTests : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("kindaworks");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+
+
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+        }
     }
 }
