4fd26ecb8cf7528d25f6981bab79b72a83b5a3ec
externalsip <93773873+externalsip@users.noreply.github.com>
2023-11-03 17:14:44 -0400

    Made some more tests on vr controllers
    I now understand how to trigger events with controllers, also managed to find a way to get their velocity, I'll make a prototype of the fishing once im done with that data

--- fishing roguelite/Assets/script/fishingRodTests.cs
+++ fishing roguelite/Assets/script/fishingRodTests.cs
@@ -1,29 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 //public XR.DirectInteractor rHand;
 //public XR.DirectInteractor lHand;
 
 
-public class fishingRodTests : MonoBehaviour
+public class fishingRodTests : XRGrabInteractable
 {
+    public GameObject rod;
+    public GameObject hook;
+    bool isHoldingItem = false;
+
+    private ControllerVelocity controllerVelocity = null;
+    private MeshRenderer meshRenderer = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        meshRenderer = GetComponent<MeshRenderer>();
+
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        controllerVelocity = args.interactor.GetComponent<ControllerVelocity>();
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("kindaworks");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         var inputDevices = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-
-
         foreach (var device in inputDevices)
-        {
-            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+        { 
+            if(isHoldingItem)
+            {
+                bool triggerValue;
+                if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+                {
+
+                }
+            }
         }
+
+    }
+
+    public void SelectTest()
+    {
+        isHoldingItem=true;
+
+    }
+    public void StopHolding()
+    {
+        isHoldingItem=false;
     }
 }
