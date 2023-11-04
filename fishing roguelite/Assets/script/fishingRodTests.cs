beebe1fd3167dff793ec383f1687ee57444c9b9e
externalsip <93773873+externalsip@users.noreply.github.com>
2023-11-04 13:13:53 -0400

    functional fishing rod prototype

--- fishing roguelite/Assets/script/fishingRodTests.cs
+++ fishing roguelite/Assets/script/fishingRodTests.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
@@ -12,27 +13,96 @@ public class fishingRodTests : XRGrabInteractable
 {
     public GameObject rod;
     public GameObject hook;
-    bool isHoldingItem = false;
-
+    private Vector3 relativeDifference;
     private ControllerVelocity controllerVelocity = null;
-    private MeshRenderer meshRenderer = null;
-
+    public GameObject hookHint;
+    bool hasJoint = true;
+    bool isThrown = false;
     protected override void Awake()
     {
         base.Awake();
-        meshRenderer = GetComponent<MeshRenderer>();
 
     }
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
-        controllerVelocity = args.interactor.GetComponent<ControllerVelocity>();
+        controllerVelocity = args.interactorObject.transform.GetComponent<ControllerVelocity>();
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
+        base.OnSelectExited(args);
+        controllerVelocity = null;
+    }
 
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+
+        if(isSelected)
+        {
+            if(updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+            {
+                HookThrow();
+            }
+        }
+    }
+
+    private void HookThrow()
+    {                            
+
+        Vector3 velocity = controllerVelocity ? controllerVelocity.Velocity : Vector3.zero;
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+        
+        foreach (var device in inputDevices)
+        {
+            bool triggerValue;
+            bool primBtnValue;
+            bool secBtnValue;
+
+            relativeDifference = hook.transform.position - rod.transform.position;
+
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+                {        
+
+                    if(device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out secBtnValue) && secBtnValue)
+                {
+                    if(isThrown == false)
+                    {
+                        var hookBody = hook.GetComponent<Rigidbody>();
+                        var hookJoint = hook.GetComponent<HingeJoint>();
+                        Destroy(hookJoint);
+                        hookBody.AddForce(velocity*50, ForceMode.Impulse);
+                        hasJoint = false;
+                        isThrown = true;
+
+                    }
+
+                }
+
+                }
+                if(device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out primBtnValue) && primBtnValue)
+            {   Debug.Log("status: " + hasJoint);
+                if (hasJoint == false)
+                {
+
+                    Debug.Log("called");
+                    var hookBody = hook.GetComponent<Rigidbody>();
+                    hookBody.velocity = Vector3.zero;
+                    hookBody.angularVelocity = Vector3.zero;
+                    hook.transform.position = hookHint.transform.position;
+                    hook.AddComponent<HingeJoint>();
+                    hook.GetComponent<HingeJoint>().connectedBody = rod.GetComponent<Rigidbody>();
+
+                    hasJoint = true;
+                    isThrown = false;
+                }
+
+            }
+
+        }
     }
 
     // Start is called before the first frame update
@@ -44,30 +114,6 @@ public class fishingRodTests : XRGrabInteractable
     // Update is called once per frame
     void Update()
     {
-
-        var inputDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-        foreach (var device in inputDevices)
-        { 
-            if(isHoldingItem)
-            {
-                bool triggerValue;
-                if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
-                {
-
-                }
-            }
-        }
-
     }
 
-    public void SelectTest()
-    {
-        isHoldingItem=true;
-
-    }
-    public void StopHolding()
-    {
-        isHoldingItem=false;
-    }
 }
